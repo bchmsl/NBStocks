54b7ad4205a1acc4885a35c739b61176d6352eb0
Bachana Mosulishvili <bachanamosulishvili@gmail.com>
2022-10-31 13:48:51 +0400

    Added Profile Fragment UI and fixed some bugs

--- app/src/main/java/com/nbstocks/nbstocks/common/extensions/ViewExtensions.kt
+++ app/src/main/java/com/nbstocks/nbstocks/common/extensions/ViewExtensions.kt
@@ -3,7 +3,6 @@ package com.nbstocks.nbstocks.common.extensions
 import android.graphics.Color
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
-import com.nbstocks.nbstocks.R
 
 fun View.makeSnackbar(s: String, isError: Boolean) {
     if (isError) {
@@ -11,4 +10,11 @@ fun View.makeSnackbar(s: String, isError: Boolean) {
     } else {
         Snackbar.make(this, s, Snackbar.LENGTH_SHORT).setBackgroundTint(Color.GREEN).show()
     }
+}
+
+fun View.disable(){
+    this.isEnabled = false
+}
+fun View.enable(){
+    this.isEnabled = true
 }
--- app/src/main/java/com/nbstocks/nbstocks/presentation/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/nbstocks/nbstocks/presentation/ui/profile/ProfileFragment.kt
@@ -1,12 +1,30 @@
 package com.nbstocks.nbstocks.presentation.ui.profile
 
-import com.nbstocks.nbstocks.presentation.ui.MainActivity
 import com.nbstocks.nbstocks.databinding.FragmentProfileBinding
 import com.nbstocks.nbstocks.presentation.ui.base.BaseFragment
 
 class ProfileFragment : BaseFragment<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
     override fun start() {
-        val activity = requireActivity() as? MainActivity
-        activity?.hideToolBar()
+        listeners()
+    }
+
+    private fun listeners() {
+        binding.apply {
+            swBalanceVisible.setOnCheckedChangeListener { buttonView, isChecked ->
+
+            }
+            tvAbout.setOnClickListener {
+
+            }
+            tvChangeEmail.setOnClickListener {
+
+            }
+            tvChangePassword.setOnClickListener {
+
+            }
+            tvLogOut.setOnClickListener {
+
+            }
+        }
     }
 }
